0c3ef43d21c7ab697ff6a5c4dd6ca28d7f53ccf5
Szymon Stryczek <szymon.stryczek@core.se>
2020-10-28 13:45:43 +0100

    init server

---
+++ radio-server.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+from flasgger import Swagger
+import modules.clock
+import os
+import string
+
+
+app = Flask(__name__)
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Projekt Ariadne",
+    "specs": [
+        {
+            "version": "0.0.1",
+            "title": "Api v1",
+            "endpoint": 'v1_spec',
+            "description": 'This is the version 1 of our API',
+            "route": '/v1/spec',
+        }
+    ]
+}
+# Create swagger definition
+swagger = Swagger(app) 
+
+@app.route('/hello')
+def hello():
+    """Hello world endpoint
+    ---
+    responses:
+      200:
+        description: Status answer
+        schema:
+            type: object
+            properties:
+              worte:
+                type: integer
+                description: Hello answer.
+    """ 
+    location = os.path.dirname(os.path.realpath(__file__))
+    return 'Hello, World '+location 
+
