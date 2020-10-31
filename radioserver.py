f5284dd908b77b2389bf457eb5fdabc2b65b84a1
Szymon Stryczek <szymon.stryczek@core.se>
2020-10-31 22:39:19 +0100

    web page

--- radioserver.py
+++ radioserver.py
@@ -1,30 +1,85 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flasgger import Swagger
-#import modules.clock
+from modules.clock import runClock, clearScreen
 import os
 import string
-
+import logging
+import threading
+import time
+from modules.statusanswer import statusAnswer
+from modules.stoppableThread import StoppableThread,ExeContext
+import subprocess
+from modules.radio import playRadio, killMusic,playLulaby
 
 app = Flask(__name__)
 app.config['SWAGGER'] = {
     "swagger_version": "2.0",
-    "title": "Projekt Ariadne",
+    "title": "Radio CLOCK APIServer",
     "specs": [
         {
             "version": "0.0.1",
-            "title": "Api v1",
+            "title": "Radio Clock Api v1",
             "endpoint": 'v1_spec',
-            "description": 'This is the version 1 of our API',
+            "description": 'This is the version 1 of radio clock API',
             "route": '/v1/spec',
         }
     ]
 }
+app.url_for('static', filename='style.css')
+
 # Create swagger definition
 swagger = Swagger(app) 
 
-@app.route('/hello')
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route('/api/hello')
 def hello():
-    """Hello world endpoint
+  """Hello world endpoint
+  ---
+  responses:
+    200:
+      description: Status answer
+      schema:
+          type: object
+          properties:
+            status:
+              type: string
+              description: Hello answer.
+  """ 
+  location = os.path.dirname(os.path.realpath(__file__))
+  return statusAnswer('Hello, World '+location)
+@app.route('/api/clock/start')
+def clockStart():
+    """Start displaying clock
+    ---
+    parameters:
+      - name: brightness
+        in: query
+        type: string
+        required: false 
+    responses:
+      200:
+        description: Status answer
+        schema:
+            type: object
+            properties:
+              status:
+                type: string
+                description: Status answer.
+    """
+    brightness = request.args.get('brightness', '')
+    if brightness == '': 
+      brightness = 0.1
+    if ExeContext.displayThread is not None:
+      ExeContext.displayThread.stop()
+    ExeContext.displayThread = StoppableThread(target=runClock, args=(brightness,))
+    ExeContext.displayThread.start()
+    return statusAnswer("Clock started")
+@app.route('/api/clock/stop')
+def clockStop():
+    """Stop displaying clock
     ---
     responses:
       200:
@@ -32,12 +87,67 @@ def hello():
         schema:
             type: object
             properties:
-              worte:
-                type: integer
-                description: Hello answer.
-    """ 
-    location = os.path.dirname(os.path.realpath(__file__))
-    return 'Hello, World '+location 
+              status:
+                type: string
+                description: Status answer.
+    """
+    if ExeContext.displayThread is not None:
+        ExeContext.displayThread.stop()
+    return statusAnswer("Clock stopped")
+@app.route('/api/radio/start')
+def radioStart():
+    """Start playing radio
+    ---
+    responses:
+      200:
+        description: Status answer
+        schema:
+            type: object
+            properties:
+              status:
+                type: string
+                description: Status answer.
+    """
+    killMusic()
+    radio = threading.Thread(target=playRadio, args=())
+    radio.start()
+    return statusAnswer("Radio started")
+@app.route('/api/music/stop')
+def radioStop():
+    """Stop playing music
+    ---
+    responses:
+      200:
+        description: Status answer
+        schema:
+            type: object
+            properties:
+              status:
+                type: string
+                description: Status answer.
+    """
+    killMusic()
+    return statusAnswer("Music stopped")
+@app.route('/api/lulaby/start')
+def lulabyStart():
+    """Start playing lulaby
+    ---
+    responses:
+      200:
+        description: Status answer
+        schema:
+            type: object
+            properties:
+              status:
+                type: string
+                description: Status answer.
+    """
+    killMusic()
+    radio = threading.Thread(target=playLulaby, args=())
+    radio.start()
+    return statusAnswer("Lulaby playing")
+ExeContext.displayThread = StoppableThread(target=runClock, args=(0.2,))
+ExeContext.displayThread.start()
 if __name__ == '__main__':
     app = create_app()
     app.run()    
